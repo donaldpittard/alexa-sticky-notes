5167f6cd58ae4b3be76174fc2f864e770cacdd8a
Donald Pittard <donaldpittard@gmail.com>
2018-08-10 07:18:19 -0400

    Initialize with slim framework and privacy page

---
+++ src/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Routes
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/privacy', function (Request $request, Response $response, array $args) {
+    $this->renderer->render($response, 'privacy.phtml', $args);
+});
