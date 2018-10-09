6d7d05e381a9ccb98f32226ae27f45d927af63ff
Donald Pittard <donaldpittard@gmail.com>
2018-10-08 21:22:36 -0400

    Implement sign out functionality
    Implement sign out functionality

--- app/components/AppHeader.js
+++ app/components/AppHeader.js
@@ -24,12 +24,20 @@ class AppHeader extends Component {
         this.hideModal();
     };
 
+    signOut() {
+        fetch('/logout')
+            .then(response => (document.location.href = response.url));
+    };
+
     render() {
         return (
             <div className="app-header">
                 <h1 className="app-header__title">Notes</h1>
                 <button className="app-header__button" type="button" onClick={this.showModal.bind(this)}>
                     <i className="fa fa-plus-square" aria-hidden="true"></i>
+                </button>
+                <button className="app-header__button app-header__signout" onClick={this.signOut}>
+                    <i className="fa fa-sign-out"></i>
                 </button>
                 <Modal show={this.state.show} handleClose={this.hideModal.bind(this)}>
                     <AddNote createNote={this.createNote.bind(this)} colors={this.props.colors} defaultColor={this.props.defaultColor} />
